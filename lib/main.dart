52c51d63cfc9a2bfb5e55ff93d1adb1af9a3ffb0
titosvq-MacPro <adlflpz@gmail.com>
2025-05-09 12:44:10 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,26 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'EBGaramond'),
+      theme: ThemeData(fontFamily: 'BebasNeue'),
       home: Scaffold(
         body: Stack(
+
           alignment: Alignment.center,
+          fit: StackFit.expand,
           children: [
-            Image(
-              image: AssetImage('assets/images/IMG_0001.jpeg'),
+            Image( // ----Widget tipo imágen
+              // image: AssetImage('assets/images/IMG_0001.jpeg'), ----Imágen local
+              image: NetworkImage('https://consejodehermandadesdeutrera.org/wp-content/uploads/2025/04/2025-04-15-estudiantes3V7A4633.jpg'),
+              fit: BoxFit.cover,
             ),
-            Text(
-              'Semana Santa de Utrera', 
-              style: TextStyle(fontSize: 40, color: Colors.white),
-              ),
+            
+            Positioned(
+              top: 85,
+              child: Text( // ----Widget tipo texto
+                'Semana Santa de Utrera', 
+                style: TextStyle(fontSize: 44, color: Colors.white),
+                ),
+            ),
           ],
         ),
       ),
